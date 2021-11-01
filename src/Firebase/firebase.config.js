d1451a6b3a3b394eb79bd783080e65d3b8085a07
SumonShakder <sumonshakder1996@gmail.com>
2021-11-01 12:34:02 +0600

    pop

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
-console.log(process.env);
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-};
-
-export default firebaseConfig;
+    apiKey: "AIzaSyBCowATU2XLkbeFf3cXu9PpvuoSABEuyhc",
+    authDomain: "react-shopping-card.firebaseapp.com",
+    projectId: "react-shopping-card",
+    storageBucket: "react-shopping-card.appspot.com",
+    messagingSenderId: "807398135967",
+    appId: "1:807398135967:web:67a114b3d8985ff5eceaaa"
+  };
+  
+  export default firebaseConfig;
+  
